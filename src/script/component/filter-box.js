666afaa58abf4f503146377b6f7c04bea249128f
Anthony Lauly <anthony.lauly1833@gmail.com>
2020-12-02 18:30:12 +0700

    continue filter-box features (unfinished)

--- src/script/component/filter-box.js
+++ src/script/component/filter-box.js
@@ -2,6 +2,7 @@ class FilterBox extends HTMLElement {
     constructor(){
         super();
         this.shadowDOM = this.attachShadow({mode:'open'});
+        this.location = [];
     }
 
     connectedCallback(){
@@ -10,35 +11,58 @@ class FilterBox extends HTMLElement {
 
     render_init(){
         this.shadowDOM.innerHTML=`
-        <div class='filter-title'>
-            Filter <br><br>
-        </div>
-        <div class='filter-category'>
-            <div class='filter-lokasi'>
-                Lokasi <br>
-                <input type="checkbox" value="Surabaya">Surabaya<br>
-                <input type="checkbox" value="Lampung">Lampung<br>
-                <input type="checkbox" value="Jakarta">Jakarta <br>
-                <input type="checkbox" value="Bandung">Bandung <br>
-                <a href="#">Lihat Selengkapnya</a>
+            <div class='filter-title'>
+                Filter <br><br>
             </div>
-            <div class='filter-harga'>
-                Harga <br>
-                <input type="text"> <br>
-                <input type="text"> <br>
+            <div class='filter-category'>
+                <div class='filter-lokasi'>
+                    Lokasi <br>
+                    <input type="checkbox" class='surabaya' value='Surabaya'> Surabaya <br>
+                    <input type="checkbox" class='lampung' value='Lampung'> Lampung <br>
+                    <input type="checkbox"> Jakarta <br>
+                    <input type="checkbox"> Bandung <br>
+                    <a href="#">Lihat Selengkapnya</a>
+                </div>
+                <div class='filter-harga'>
+                    Harga <br>
+                    <input type="text"> <br>
+                    <input type="text"> <br>
+                </div>
+                <div class='filter-industri'>
+                    Industri <br>
+                    <input type="checkbox"> Agriculture <br>
+                    <input type="checkbox"> Food & Beverages <br>
+                    <input type="checkbox"> Hotel,Tourism, & Catering <br>
+                    <input type="checkbox"> Textile, Clothing, & Footwear <br>
+                    <a href="#">Lihat Selengkapnya</a>
+                </div>
             </div>
-            <div class='filter-industri'>
-                Industri <br>
-                <input type="checkbox"> Agriculture <br>
-                <input type="checkbox"> Food & Beverages <br>
-                <input type="checkbox"> Hotel,Tourism, & Catering <br>
-                <input type="checkbox"> Textile, Clothing, & Footwear <br>
-                <a href="#">Lihat Selengkapnya</a>
-            </div>
-        </div>
         `;
+        location_array = this.location;
+        const surabaya = this.shadowDOM.querySelector('.surabaya');
+        surabaya.addEventListener('change', function(){
+            if (this.checked) {
+                location_array.push('Surabaya');              
+            } 
+        });
 
-        console.log(this.shadowDOM.querySelectorAll('input'));
+        const lampung = this.shadowDOM.querySelector('.lampung');
+        lampung.addEventListener('change', function(){
+            if (this.checked) {
+                location_array.push('Lampung');
+              } 
+        });
+        console.log(location_array);
+        this.location = location_array;
+    }
+
+
+    attributeChangedCallback(name, oldval, newval){
+        console.log('attributes changed');
+    }
+
+    static get observedAttributes(){
+        return ['surabaya_check']
     }
 }
 
