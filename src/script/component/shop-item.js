7368e96d4ce299acae39511fea8254cee04e617d
Anthony Lauly <anthony.lauly1833@gmail.com>
2020-12-03 13:41:54 +0700

    successfully add flipping animation in shop-item

--- src/script/component/shop-item.js
+++ src/script/component/shop-item.js
@@ -27,13 +27,68 @@ class ShopItem extends HTMLElement {
                     margin-bottom: 1px;
                     padding-bottom: 1px;
                 }
+                .flip-box {
+                background-color: transparent;
+                width: 200px;
+                height: 130px;
+                border: 1px solid #f1f1f1;
+                perspective: 1000px; /* Remove this if you don't want the 3D effect */
+                }
+
+                /* This container is needed to position the front and back side */
+                .flip-box-inner {
+                position: relative;
+                width: 100%;
+                height: 100%;
+                text-align: center;
+                transition: transform 0.8s;
+                transform-style: preserve-3d;
+                }
+
+                /* Do an horizontal flip when you move the mouse over the flip box container */
+                .flip-box:hover .flip-box-inner {
+                transform: rotateY(180deg);
+                }
+
+                /* Position the front and back side */
+                .flip-box-front, .flip-box-back {
+                position: absolute;
+                width: 100%;
+                height: 100%;
+                -webkit-backface-visibility: hidden; /* Safari */
+                backface-visibility: hidden;
+                }
+
+                /* Style the front side (fallback if image is missing) */
+                .flip-box-front {
+                background-color: #bbb;
+                color: black;
+                }
+
+                /* Style the back side */
+                .flip-box-back {
+                background-color: #65645F;
+                color: white;
+                transform: rotateY(180deg);
+                }
             </style>
         `;
     }
 
     render(){
         this.innerHTML += `
-            <img src="${this.shop.logo}" alt="${this.shop.name}">
+            <div class="flip-box">
+                <div class="flip-box-inner">
+                    <div class="flip-box-front">
+                    <img src="${this.shop.logo}" alt="${this.shop.name}">
+                    </div>
+                    <div class="flip-box-back">
+                    <h3>${this.shop.industry} Industry</h3>
+                    <h3>Since ${this.shop.business_start}</h3>
+                    <h3>${this.shop.units} units</h3>
+                    </div>
+                </div>
+            </div>
             <div float='right'>
                 <h2>${this.shop.name}</h2>
                 <h3>${this.shop.headquarter}</h3>
