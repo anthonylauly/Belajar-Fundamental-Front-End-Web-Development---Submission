65d1aad6f212c946435576905124e25f13cabcb9
Anthony Lauly <anthony.lauly1833@gmail.com>
2020-12-04 14:45:50 +0700

    Done modifying Header, working on sign-in feature

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import "./styles/css/shop.css";
+import "./styles/css/footer.css";
 import "./styles/css/content.css";
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/js/bootstrap.bundle"
 import main from "./script/view/main.js";
 
 document.addEventListener("DOMContentLoaded", main);
--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -16,6 +16,25 @@ class DataSource {
             }
         })
     };
+
+    static retreiveMyShops(username, password){
+        return fetch("https://cabangku.azurewebsites.net/api/shop/", {
+            headers: {
+                "Authorization": "Basic "  + base64.encode(username + ":" + password),
+            }
+        })
+        .then(response => {
+            return response.json();
+        })
+        .then(responseJson => {
+            console.log(responseJson);
+            if(responseJson) {
+                return Promise.resolve(responseJson);
+            } else {
+                return Promise.reject("Result is not found");
+            }
+        })
+    }
 }
 
 export default DataSource;
